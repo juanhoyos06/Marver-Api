f2a92f4244568f460b3a894cec038aa043e9423e
juanhoyos06 <jphoyos0610@gmail.com>
2023-09-22 08:34:14 -0500

    feat: creacion proyecto

---
+++ layouts/default.vue
@@ -0,0 +1,34 @@
+<template>
+    <v-toolbar dark prominent>
+      <v-app-bar-nav-icon></v-app-bar-nav-icon>
+
+      <v-toolbar-title>
+        <v-img src="https://1000marche.net/wp-content/uploads/2020/03/Marvel-logo.png" width="170">
+
+        </v-img>
+      </v-toolbar-title>
+
+      <v-spacer></v-spacer>
+
+      <v-btn icon>
+        <v-icon>mdi-export</v-icon>
+      </v-btn>
+    </v-toolbar>
+    
+
+</template>
+<script setup>
+const titleApp = ref("App de Tareas")
+let likes = ref(0)
+
+const incrementLikes = () => {
+  // NOTA: Recuerda acceder al valor de la propiedad con .value
+  likes.value++
+}
+</script>
+
+<style>
+.links {
+  margin: 15px;
+}
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+
+  </div>
+</template>
+<script>
+const marvel = {
+  render: () => {
+    const urlAPI = '';
+    let contentHTML = '';
+
+    fetch(urlAPI)
+      .then(res => res.json())
+      .then((json) => {
+        console.log(json, 'RES.JSON');
+      })
+  }
+};
+marvel.render
+
+</script>
